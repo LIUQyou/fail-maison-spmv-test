127a53c6ba6b9a240daad8e25a0d63db1e75815f
LIUQyou <1343451020@qq.com>
2022-10-13 11:19:30 +0200

    TLBhuge3

--- main.c
+++ main.c
@@ -13,6 +13,56 @@
 #include "utils.h"
 #include "config.h"
 
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define MB_1 (1024*1024)
+#define GB_1 (1024*MB_1)
+#define MB_8 (8*MB_1)
+
+int M, N, entries, anz, i, j, *row, *col, *row_ptr, *colind, *offset, *indices, num_diags, stride, num_cols;
+MYTYPE *x, *y, *val, *dia_data, *ell_data, *coo_val;
+  
+void init_hugetlb_seg(int **vec,int size)
+{
+  int shmid1;
+  shmid1 = shmget(rand(), size*sizeof(int), SHM_HUGETLB
+         | IPC_CREAT | SHM_R
+         | SHM_W);
+  if ( shmid1 < 0 ) {
+    perror("shmget");
+    exit(1);
+  }
+  printf("HugeTLB shmid: 0x%x\n", shmid1);
+  *vec = shmat(shmid1, 0, 0);
+  if (*vec == (int *)-1) {
+    printf("Shared memory attach failure");
+    shmctl(shmid1, IPC_RMID, NULL);
+    exit(2);
+  }
+  printf("address is %p\n",*vec);
+}
+
+void init_hugetlb_seg_mytype(MYTYPE **vec,int size)
+{
+  int shmid1;
+  shmid1 = shmget(rand(), size*sizeof(MYTYPE), SHM_HUGETLB
+         | IPC_CREAT | SHM_R
+         | SHM_W);
+  if ( shmid1 < 0 ) {
+    perror("shmget");
+    exit(1);
+  }
+  printf("HugeTLB shmid: 0x%x\n", shmid1);
+  *vec = shmat(shmid1, 0, 0);
+  if (*vec == (MYTYPE *)-1) {
+    printf("Shared memory attach failure");
+    shmctl(shmid1, IPC_RMID, NULL);
+    exit(2);
+  }
+  printf("address is %p\n",*vec);
+}
+
 // For hugebubbles-00010 on the m400, the MPKI is
 // 26.6 for EXE_TIME 20.0 s
 // 24.2 for EXE_TIME 10.0 s
@@ -25,8 +75,6 @@ int main(int argc, char* argv[])
   int ret_code, r, c, k;
   MM_typecode matcode;
   FILE *f, *g;
-  int M, N, entries, anz, i, j, *row, *col, *row_ptr, *colind, *offset, *indices, num_diags, stride, num_cols;
-  MYTYPE *x, *y, *val, *dia_data, *ell_data, *coo_val;
   double v;
   clock_t start, stop;
   double sum = 0, mean = 0, sd, variance = 0;
@@ -69,45 +117,60 @@ int main(int argc, char* argv[])
     M = N;
   #endif
 
-  row = (int*)malloc(anz * sizeof(int));
+  //row = (int*)malloc(anz * sizeof(int));
+  init_hugetlb_seg(&row,anz);
   if(row == NULL){
-    fprintf(stderr, "couldn't allocate row using malloc");
+    fprintf(stderr, "couldn't allocate row using malloc\n");
     exit(1);
   }
-  col = (int*)malloc(anz * sizeof(int));
+  
+  //col = (int*)malloc(anz * sizeof(int));
+  init_hugetlb_seg(&col,anz);
   if(col == NULL){
-    fprintf(stderr, "couldn't allocate col using malloc");
+    fprintf(stderr, "couldn't allocate col using malloc\n");
     exit(1);
   }
-  coo_val = (MYTYPE*)malloc(anz * sizeof(MYTYPE));
+  
+  //coo_val = (MYTYPE*)malloc(anz * sizeof(MYTYPE));
+  init_hugetlb_seg_mytype(&coo_val,anz);
   if(coo_val == NULL){
-    fprintf(stderr, "couldn't allocate val using malloc");
+    fprintf(stderr, "couldn't allocate val using malloc\n");
     exit(1);
   }
+  
   row_ptr = (int*)calloc(N+1, sizeof(int));
+  //init_hugetlb_seg(&row_ptr,N+1);
   if(row_ptr == NULL){
-    fprintf(stderr, "couldn't allocate row_ptr using malloc");
+    fprintf(stderr, "couldn't allocate row_ptr using malloc\n");
     exit(1);
   }
-  colind = (int*)malloc(anz * sizeof(int));
+  
+  //colind = (int*)malloc(anz * sizeof(int));
+  init_hugetlb_seg(&colind,anz);
   if(colind == NULL){
-    fprintf(stderr, "couldn't allocate colind using malloc");
+    fprintf(stderr, "couldn't allocate colind using malloc\n");
     exit(1);
   }
-  val = (MYTYPE*)malloc(anz * sizeof(MYTYPE));
+  
+  //val = (MYTYPE*)malloc(anz * sizeof(MYTYPE));
+  init_hugetlb_seg_mytype(&val,anz);
   if(val == NULL){
-    fprintf(stderr, "couldn't allocate val using malloc");
+    fprintf(stderr, "couldn't allocate val using malloc\n");
     exit(1);
   }
-  x=(MYTYPE*) malloc( sizeof(MYTYPE)*N );
+  
+  //x=(MYTYPE*) malloc( sizeof(MYTYPE)*N );
+  init_hugetlb_seg_mytype(&x,N);
   if(x == NULL){
-    fprintf(stderr, "couldn't allocate x using malloc");
+    fprintf(stderr, "couldn't allocate x using malloc\n");
     exit(1);
   }
   init_arr(N,x);
-  y=(MYTYPE*) calloc(N, sizeof(MYTYPE));
+  
+  //y=(MYTYPE*) calloc(N, sizeof(MYTYPE));
+  init_hugetlb_seg_mytype(&y,N);
   if(y == NULL){
-    fprintf(stderr, "couldn't allocate y using calloc");
+    fprintf(stderr, "couldn't allocate y using calloc\n");
     exit(1);
   }
   
@@ -326,14 +389,8 @@ int main(int argc, char* argv[])
     fclose(f);
   #endif
 
-  free(x);
-  free(y);
+  //free(y);
   free(row_ptr);
-  free(row);
-  free(col);
-  free(coo_val);
-  free(colind);
-  free(val);
 
   return 0;
 }
